4896a6bf201dd92f9cda7864f987f1df5d3b856e
Chaeheon Lee <mickey050110@naver.com>
2024-10-02 23:26:11 +0900

    수정 테스트

--- python/백준/1000번 A+B.py
+++ python/백준/1000번 A+B.py
@@ -1,3 +1,5 @@
 a, b = input().split()
 
 print(int(a)+int(b))
+
+#이 부분 수정
--- python/백준/2439번 별 찍기 - 2.py
+++ python/백준/2439번 별 찍기 - 2.py
@@ -1,5 +1,13 @@
+#내 풀이
+
+# N = int(input())
+# i = 1
+# while i<=N:
+#     print("{0:>{1}}" .format("*"*i,N))
+#     i+=1
+
+#코파일럿 풀이
+
 N = int(input())
-i = 1
-while i<=N:
-    print("{0:>{1}}" .format("*"*i,N))
-    i+=1
+for i in range(1, N + 1):
+    print(" " * (N - i) + "*" * i)
