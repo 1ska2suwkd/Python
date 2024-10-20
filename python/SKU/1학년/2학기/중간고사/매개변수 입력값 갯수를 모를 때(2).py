2a4ff25cd3e6b86169edd074507b54652011620c
1ska2suwkd <mickey050110@naver.com>
2024-10-21 00:23:19 +0900

    Create 매개변수 입력값 갯수를 모를 때(2).py

---
+++ python/SKU/1학년/2학기/중간고사/매개변수 입력값 갯수를 모를 때(2).py
@@ -0,0 +1,10 @@
+def add_mul(choice,*args):
+    if choice == "add":
+        result = 0
+        for i in args: resilt += i
+    elif choice == "mul":
+        result = 1
+        for i in args: result *= i
+    return result
+result = add_mul('add',1,2,3,4,5)
+print(result)
