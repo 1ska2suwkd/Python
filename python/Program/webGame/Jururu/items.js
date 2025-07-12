7e5ecc1ebd3ebd1c796fd204490975742fc5c6dc
1ska2suwkd <mickey050110@naver.com>
2025-07-12 09:56:15 +0900

    Create items.js

---
+++ python/Program/webGame/Jururu/items.js
@@ -0,0 +1,12 @@
+//게임의 존재하는 모든 아이템의 데이터
+const masterItemList = [
+    {id: 1, name: 'STAR!', description: "강한 힘이 느껴진다.", frame: 11, grade: 'S'},
+    {id: 2, name: '하트', description: "누군가의 사랑이 담겨있다.", frame: 658, grade: 'A'},
+    {id: 3, name: '귀여운 옷', description: "딱 내 취향이다.", frame: 2106, grade: 'B'},
+    {id: 4, name: '네잎클로버', description: "럭키!", frame: 667, grade: 'C'},
+    {id: 5, name: '도넛', description: "맛있는 도넛! 먹으면 힘이 솟는다.", frame: 527, grade: 'C'},
+    {id: 6, name: '용사의 검', description: "다룰줄은 모른다.", frame: 1702, grade: 'C'},
+    {id: 7, name: '뼈다귀', description: "단단하다.", frame: 230, grade: 'D'}
+]
+
+export default masterItemList;
